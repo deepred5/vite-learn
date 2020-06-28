53513c3f076216b2b97ceabdaf4d7496657dc988
tiancong <tiancong@hujiang.com>
2020-06-28 17:06:39 +0800

    docs: 添加vscode debug启动方法

---
+++ playground/mydemo/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 @click="count++">hello vite {{count}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
